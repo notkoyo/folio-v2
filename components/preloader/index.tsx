cf0a3d9f0e2d38c7c77ec59a8fb4bbd8384bd823
notkoyo <kaiden01@outlook.de>
2024-07-06 03:55:48 +0100

    add stagger to loader

--- components/preloader/index.tsx
+++ components/preloader/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { motion, useAnimation } from "framer-motion";
+import {useEffect, useState} from "react";
+import {easeIn, motion, useAnimation} from "framer-motion";
 
 export default function PreLoader() {
   const [showWelcome, setShowWelcome] = useState<boolean>(false);
@@ -14,11 +14,11 @@ export default function PreLoader() {
     const loadingSequence = async () => {
       await controls.start({
         width: "100%",
-        transition: { duration: 1.8 },
+        transition: {duration: 1.8},
       });
       await controls.start({
         opacity: 0,
-        transition: { duration: 0.3 },
+        transition: {duration: 0.3},
       });
       await controls.start({
         display: "none",
@@ -29,7 +29,7 @@ export default function PreLoader() {
     const screenUp = async () => {
       await screenControls.start({
         y: "-100%",
-        transition: { duration: 0.55 },
+        transition: {duration: 0.55},
       });
 
       document.documentElement.classList.remove("overflow-y-hidden");
@@ -39,11 +39,13 @@ export default function PreLoader() {
 
     setTimeout(() => {
       screenUp();
-    }, 3300);
+    }, 3600);
   }, [controls, screenControls]);
 
   return (
-    <motion.div animate={screenControls} className="h-screen w-screen flex justify-center items-center flex-col bg-[#191815] absolute top-0 left-0 z-[999]">
+    <motion.div
+      animate={screenControls}
+      className="h-screen w-screen flex justify-center items-center flex-col bg-[#191815] absolute top-0 left-0 z-[999]">
       <div className={`${showWelcome ? "w-screen flex justify-center" : "w-[18rem]"}`}>
         <motion.div
           animate={controls}
@@ -54,15 +56,16 @@ export default function PreLoader() {
             borderRadius: "9999px",
           }}
         />
-        {showWelcome && (
-          <motion.span
-            initial={{ opacity: 0, y: 50 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5 }}
-            className="uppercase font-bold text-9xl text-[#e8e8e8]">
-            welcome
-          </motion.span>
-        )}
+        {showWelcome &&
+          ["w", "e", "l", "c", "o", "m", "e"].map((letter, i) => (
+            <motion.span
+              initial={{opacity: 0, y: 50}}
+              animate={{opacity: 1, y: 0}}
+              transition={{duration: 0.45, delay: 0.05 * i}}
+              className="uppercase font-bold text-9xl text-[#e8e8e8]">
+              {letter}
+            </motion.span>
+          ))}
       </div>
     </motion.div>
   );
